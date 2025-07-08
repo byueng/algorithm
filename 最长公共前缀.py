29f784ed9141ee27a434c6fc47cd7b2c1084747a
jwm <wzskqjh@gmail.com>
2025-07-08 22:15:52 +0800

    complete longestCommonPrefix

--- 最长公共前缀.py
+++ 最长公共前缀.py
@@ -9,9 +9,33 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        
-        return ""          
-
+        commonprefix: str = ""
+        current_char: str = ""
+        strs_length: int = len(strs)
+        epoch_num: int = 0
+        flag: bool = True
+        # singal char 
+        if strs_length == 1 :
+            commonprefix = strs[0]
+            flag = False
+        while flag: 
+            for i in range(strs_length - 1):
+                try:
+                    left_char = strs[i][epoch_num] if strs[i] != "" else ""
+                    right_char = strs[i+1][epoch_num] if strs[i+1] != "" else ""
+                    current_char = left_char
+                except IndexError:
+                    return commonprefix
+                # null char 
+                if left_char == "" or right_char == "":
+                    return ""
+                if left_char != right_char:
+                    flag = False
+                    break
+            if flag: 
+                commonprefix += current_char
+                epoch_num += 1    
+        return commonprefix      
 
 def main():
     strs = ["dog","racecar","car"]
