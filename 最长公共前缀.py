e7fe6a8fe2bc559a2983f89ed2e7bac5feb8acdd
jwm <wzskqjh@gmail.com>
2025-07-08 22:14:25 +0800

    complete longestCommonPrefix

---
+++ 最长公共前缀.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025-07-08 20:26
+# @Author  : jwm
+# @File    : longestCommonPrefix.py
+# @description: For the High King
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        return ""          
+
+
+def main():
+    strs = ["dog","racecar","car"]
+    solution = Solution()
+    result = solution.longestCommonPrefix(strs)
+    print(result)
+
+if __name__ == "__main__":
+    main()
